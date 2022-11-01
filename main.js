380b12eec2e1c0e6dd6f686ee5a9c9e1a1eb035b
markrusselldev <mark@markrussell.dev>
2022-10-31 23:57:34 -0600

    FreeCodeCamp stories edits, Added Twitter button

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-  $('#get-quote').on('click', () => {
+  $('#new-quote').on('click', () => {
     jQueryFromJSON();
   });// End .on('click')
 
@@ -63,9 +63,37 @@ function jQueryFromJSON() {
       });
 
       // Build our interface
-      $('#quote').text(obj.content);
+      $('#text').text(obj.content);
       $('#author').text(obj.author);
       $('#tags').text("#" + tagsDisplay);
+
+      // Build our Twitter links
+      const encoded = encodeURIComponent('\"' + obj.content + ' -' + obj.author);
+      //console.log(encoded);
+     
+      // Capitalize first letter of each tag
+      let capitalizedTags = tags.map(element => {
+        return element.charAt(0).toUpperCase() + element.slice(1).toLowerCase();
+      });
+      //console.log(capitalizedTags);
+      // Hashtags cannot contain hypens
+      // stringify the object, replace, then parse it
+      // Capitalize first letter after '-' hypens, then remove hypens
+      // e.g. famous-quotes is now FamousQuotes
+      let tweetTags = JSON.parse(
+        JSON.stringify(capitalizedTags).replace(/(?:^|-)\S/g, a => a.toUpperCase()).replace('-', '')
+      );
+
+      //console.log(tags);
+      //console.log(typeof tags);
+      //console.log(tweetTags);
+      let tweetLink = `https://twitter.com/intent/tweet?text=${encoded}&hashtags=${tweetTags}&related=freecodecamp`;
+      console.log(tweetLink)
+
+      // Add image url to twitter meta
+      $("meta[property='twitter\\image']").attr("content", url);
+      // Add tweeLink to tweet button
+      $("#tweet-quote").attr('href', tweetLink);
 
       /*
        * Official Unsplash API JSON call
